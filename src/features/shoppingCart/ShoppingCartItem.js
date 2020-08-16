f51b9bda830df0462dd688649d95f56cb8602950
Wagner Doering <wdoering@k-lagan.com>
2020-08-16 17:26:30 +0200

    Using media query to reorganize items of card.

--- src/features/shoppingCart/ShoppingCartItem.js
+++ src/features/shoppingCart/ShoppingCartItem.js
@@ -1,122 +1,92 @@
 import React from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import PropTypes from "prop-types";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
-import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import SkipNextIcon from "@material-ui/icons/SkipNext";
-import IconButton from "@material-ui/core/IconButton";
 import { useDispatch } from "react-redux";
 import { removeItem } from "../shoppingCart/shoppingCartSlice";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    margin: 5,
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  poster: {
-    minWidth: 150,
-    height: 220,
-    maxWidth: 180,
-  },
-  controls: {
-    display: "flex",
-    alignItems: "center",
-    paddingTop: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingBottom: theme.spacing(1),
-  },
-  playIcon: {
-    height: 38,
-    width: 38,
-  },
-}));
+const ShoppingCartItem = (props) => {
+  const matches = useMediaQuery("(max-width:375px)");
 
-export default function ShoppingCartItem(props) {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: matches ? "column" : "row",
+      justifyContent: "space-between",
+    },
+    detailsRight: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      margin: 5,
+      minWidth: 180,
+    },
+    centralContent: {
+      display: "flex",
+      flexGrow: 1,
+      flexDirection: "column",
+      justifyContent: "center",
+      margin: 5,
+    },
+    poster: {
+      width: 270,
+      height: 380,
+      margin: 5,
+    },
+    controls: {
+      display: "flex",
+      alignItems: "center",
+      paddingTop: theme.spacing(1),
+      paddingLeft: theme.spacing(2),
+      paddingBottom: theme.spacing(1),
+    },
+  }));
   const classes = useStyles();
   const dispatch = useDispatch();
   const { media } = props;
 
+  console.log(matches);
   return (
     <Card className={classes.root}>
-      <div className={classes.details}>
-        <CardMedia
-          className={classes.poster}
-          component="img"
-          src={`http://image.tmdb.org/t/p/w500//${media.poster_path}`}
-          title="Live from space album cover"
-        />
-        <div className={classes.controls}>
-          {/* <Typography component="h5" variant="h5">
-            $ 1.99
-          </Typography>
-          <IconButton>
-            <DeleteIcon></DeleteIcon>
-          </IconButton> */}
-          <Button
-            data-testid="addToCartButton"
-            variant="outlined"
-            size="large"
-            color="primary"
-            onClick={() => dispatch(removeItem(media))}
-          >
-            Delete
-            <DeleteIcon />
-          </Button>
-          {/* <IconButton aria-label="previous">
-            {theme.direction === "rtl" ? (
-              <SkipNextIcon />
-            ) : (
-              <SkipPreviousIcon />
-            )}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon className={classes.playIcon} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === "rtl" ? (
-              <SkipPreviousIcon />
-            ) : (
-              <SkipNextIcon />
-            )}
-          </IconButton> */}
-        </div>
-      </div>
-
-      <CardContent className={classes.content}>
+      <CardMedia
+        className={classes.poster}
+        component="img"
+        src={`http://image.tmdb.org/t/p/w500//${media.poster_path}`}
+        title="Live from space album cover"
+      />
+      <div className={classes.centralContent}>
         <Typography component="h5" variant="h5">
           {media.title}
         </Typography>
         <Typography variant="subtitle1" color="textSecondary">
           {media.release_date.substring(0, 4)}
         </Typography>
-        <Typography component="h5" variant="h5">
-          $ 1.99
-        </Typography>
-        {/* <IconButton>
-          <DeleteIcon></DeleteIcon>
-        </IconButton>
+      </div>
+      <div className={classes.detailsRight}>
         <Button
           data-testid="addToCartButton"
           variant="outlined"
           size="large"
           color="primary"
+          onClick={() => dispatch(removeItem(media))}
         >
           Delete
           <DeleteIcon />
-        </Button> */}
-      </CardContent>
+        </Button>
+      </div>
     </Card>
   );
-}
+};
+ShoppingCartItem.propTypes = {
+  media: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+  }),
+};
+export default ShoppingCartItem;
