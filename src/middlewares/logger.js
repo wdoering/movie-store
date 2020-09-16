2dccfa5c0957dd49e0d4618cddd6326c5bd3a262
Wagner Doering <wdoering@k-lagan.com>
2020-09-16 13:25:42 +0200

    Added cypress.

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -10,4 +10,19 @@ const logger = (store) => (next) => (action) => {
   return result;
 };
 
+// const genreLogger = (store) => (next) => (action) => {
+//   console.group(action.type);
+//   console.info("dispatching", action);
+//   let result = next(action);
+//   console.log("next state", store.getState());
+//   console.groupEnd();
+//   if (action.type === "genre/setGenreList") {
+//     console.log("deu bom!");
+//     return true;
+//   }
+//   console.log("deu ruim!");
+//   return false;
+//   //   return result;
+// };
+
 export default logger;
---
+++ src/middlewares/logger.test.js
@@ -0,0 +1,6 @@
+describe("make sure subscribe to genre reducer", () => {
+  it("should return true when genre/setGenreList is called", () => {
+    // funny that the quickest way I found to a solution was using
+    // integration tests and not unit testing
+  });
+});
